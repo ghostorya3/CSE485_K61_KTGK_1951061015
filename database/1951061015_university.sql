646fefbef9e0f61490ad710d59c11121fb70a83a
ghostorya3 <88933306+ghostorya3@users.noreply.github.com>
2022-01-06 16:26:49 +0700

    Add files via upload

---
+++ database/1951061015_university.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th1 06, 2022 lúc 10:03 AM
+-- Phiên bản máy phục vụ: 10.4.21-MariaDB
+-- Phiên bản PHP: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `1951061015_university`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `monhoc`
+--
+
+CREATE TABLE `monhoc` (
+  `mamh` int(10) UNSIGNED NOT NULL,
+  `ten_mh` varchar(100) NOT NULL,
+  `sotinchi` int(10) NOT NULL,
+  `sotiet_lt` int(10) NOT NULL,
+  `sotiet_bt` int(10) NOT NULL,
+  `sotiet_thtn` int(10) NOT NULL,
+  `sogio_tuhoc` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Đang đổ dữ liệu cho bảng `monhoc`
+--
+
+INSERT INTO `monhoc` (`mamh`, `ten_mh`, `sotinchi`, `sotiet_lt`, `sotiet_bt`, `sotiet_thtn`, `sogio_tuhoc`) VALUES
+(1, 'Tiếng Anh', 12, 6, 5, 7, 8),
+(2, 'Tiếng Hàn', 13, 6, 5, 8, 8),
+(3, 'Toán', 14, 6, 5, 7, 7),
+(4, 'Vật lý', 13, 6, 5, 7, 8);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
